7a1a810d565e97ec3899ce7aa1ec84cb3e138614
Buriak-byte <oleksii.buriak12@gmail.com>
2026-03-04 17:42:18 +0200

    Update MMJS.js

--- MMJS.js
+++ MMJS.js
@@ -1,20 +1,62 @@
-//1
-const friends = ["mango", "poly", "ajax", "kivi"]
-let string = "" 
-let h = 0
-//1.1
-for (h = 0; h < friends.length; h++){
-    string += friends[h]
-   if(h !== friends.length - 1){
-    string += ","
-   }
-}
+// //1
+// const friends = ["mango", "poly", "ajax", "kivi"]
+// let string = "" 
+// let h = 0
+// //1.1
+// for (h = 0; h < friends.length; h++){
+//     string += friends[h]
+//    if(h !== friends.length - 1){
+//     string += ","
+//    }
+// }
 
-    console.log(string)
-    //1.2
-console.log(friends.join(","))
-//2
-// Працюємо з колекцією карток в trello. Метод splice() (можна використати інші методи)
-const cards = ["карточка1", "карточка2", "карточка3", "карточка4", "карточка5"]
- const cardToRemote = "карточка3"
- console.log()
+//     console.log(string)
+//     //1.2
+// console.log(friends.join(","))
+// //2
+// const cards = ["карточка1", "карточка2", "карточка3", "карточка4", "карточка5"]
+// //3
+//  const cardToRemote = "карточка3"
+
+//  const i = cards.indexOf(cardToRemote)
+// console.log(cards.splice(i, 1));
+// //4
+// const cardToInsert = "карточка 6"
+// console.log(cards.push(cardToInsert))
+// //5
+// const cardToUpdate = "карточка4"
+// const i2 = cards.indexOf(cardToUpdate)
+// console.log(cards.splice(i2, 1, "карточка 4.2"))
+// console.log(cards)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
